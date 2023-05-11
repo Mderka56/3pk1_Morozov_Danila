2bbebe937a9a8a249697a9bf81e673b1eeb433b4
Mderka56 <mderka@yandex.ru>
2023-05-11 23:57:05 +0500

    kt

--- kt/kt/MainWindow.xaml.cs
+++ kt/kt/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Ink;
 
 
 namespace kt
@@ -27,9 +28,12 @@ namespace kt
         public string filePath;
         public bool isTextChanged;
 
-        public MainWindow()
+        public MainWindow(string filePath)
         {
             InitializeComponent();
+
+
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -62,127 +66,116 @@ namespace kt
                 IC.EditingMode = InkCanvasEditingMode.Ink;
         }
 
-        //private void Button_Click_2(object sender, RoutedEventArgs e)
-        //{
-        //    if (string.IsNullOrEmpty(filePath))
-        //    {
-        //        SaveFileDialog saveFileDialog = new SaveFileDialog();
-
-        //        if (saveFileDialog.ShowDialog() == true)
-        //        {
-        //            filePath = saveFileDialog.FileName;
-        //        }
-        //    }
-        //    File.WriteAllText($"{filePath}.txt", TxtTb.Text);
-        //    isTextChanged = false;
-        //}
-
-        //private void SaveJpgBut_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-        //    dlg.FileName = "savedimage";
-        //    dlg.DefaultExt = ".jpg";
-        //    dlg.Filter = "Image (.jpg)|*.jpg";
-
-        //    Nullable<bool> result = dlg.ShowDialog();
-
-        //    if (result == true)
-        //    {
-        //        string filename = dlg.FileName;
-        //        int margin = (int)this.IC.Margin.Left;
-        //        int width = (int)this.IC.ActualWidth - margin;
-        //        int height = (int)this.IC.ActualHeight - margin;
-        //        RenderTargetBitmap rtb =
-        //        new RenderTargetBitmap(width, height, 94d, 92d, PixelFormats.Default);
-        //        rtb.Render(IC);
-
-        //        using (FileStream fs = new FileStream(filename, FileMode.Create))
-        //        {
-        //            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-        //            encoder.Frames.Add(BitmapFrame.Create(rtb));
-        //            encoder.Save(fs);
-        //        }
-        //    }
-        //}
-
-
-        // Короче я пытался но видимо плохо пытался
-        class Memento
-        //вроде как управляет механизмом сохранения и восстановления состояний
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            public string State { get; private set; }
-            public Memento(string state)
+            if (string.IsNullOrEmpty(filePath))
             {
-                this.State = state;
-            }
-        }
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
 
-        class Caretaker
-            //хранит информацию о состоянии
-        {
-            public Memento Memento { get; set; }
-        }
-
-        class Originator
-        {
-            //вроде как хранит и выполняет все методы, сохраняет состояние и восстанавливает его
-            public string State { get; set; }
-            public void SetMemento(Memento memento)
-            {
-                State = memento.State;
-            }
-            public Memento CreateMemento()
-            {
-                return new Memento(State);
-            }
-
-
-            private void Button_Click_2(object sender, RoutedEventArgs e)
-            {//за пределами хранителя это работает
-                if (string.IsNullOrEmpty(filePath))
+                if (saveFileDialog.ShowDialog() == true)
                 {
-                    SaveFileDialog saveFileDialog = new SaveFileDialog();
-
-                    if (saveFileDialog.ShowDialog() == true)
-                    {
-                        filePath = saveFileDialog.FileName;
-                    }
-                }
-                File.WriteAllText($"{filePath}.txt", TxtTb.Text);
-                isTextChanged = false;
-            }
-            public void SaveJpgBut() //за пределами хранителя это работает
-            {
-                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-                dlg.FileName = "savedimage";
-                dlg.DefaultExt = ".jpg";
-                dlg.Filter = "Image (.jpg)|*.jpg";
-
-                Nullable<bool> result = dlg.ShowDialog();
-
-                if (result == true)
-                {
-                    string filename = dlg.FileName;
-                    int margin = (int)this.IC.Margin.Left;
-                    int width = (int)this.IC.ActualWidth - margin;
-                    int height = (int)this.IC.ActualHeight - margin;
-                    RenderTargetBitmap rtb =
-                    new RenderTargetBitmap(width, height, 94d, 92d, PixelFormats.Default);
-                    rtb.Render(IC);
-
-                    using (FileStream fs = new FileStream(filename, FileMode.Create))
-                    {
-                        BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-                        encoder.Frames.Add(BitmapFrame.Create(rtb));
-                        encoder.Save(fs);
-                    }
+                    filePath = saveFileDialog.FileName;
                 }
             }
+            File.WriteAllText($"{filePath}.txt", TxtTb.Text);
+            isTextChanged = false;
         }
+
+        private void SaveJpgBut_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "savedimage";
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "Image (.jpg)|*.jpg";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            using (FileStream fs = new FileStream(dlg.FileName, FileMode.Create))
+            {
+
+                IC.Strokes.Save(fs);
+            }
+        }
+
+        //Дальше не нужно и не работает
+
+        
+        //// Короче я пытался но видимо плохо пытался
+        //class Memento
+        ////вроде как управляет механизмом сохранения и восстановления состояний
+        //{
+        //    public string State { get; private set; }
+        //    public Memento(string state)
+        //    {
+        //        this.State = state;
+        //    }
+        //}
+
+        //class Caretaker
+        ////хранит информацию о состоянии
+        //{
+        //    public Memento Memento { get; set; }
+        //}
+
+        //class Originator
+        //{
+        //    //вроде как хранит и выполняет все методы, сохраняет состояние и восстанавливает его
+        //    public string State { get; set; }
+        //    public void SetMemento(Memento memento)
+        //    {
+        //        State = memento.State;
+        //    }
+        //    public Memento CreateMemento()
+        //    {
+        //        return new Memento(State);
+        //    }
+
+
+        //    private void Button_Click_2(object sender, RoutedEventArgs e)
+        //    {//за пределами хранителя это работает
+        //        if (string.IsNullOrEmpty(filePath))
+        //        {
+        //            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+        //            if (saveFileDialog.ShowDialog() == true)
+        //            {
+        //                filePath = saveFileDialog.FileName;
+        //            }
+        //        }
+        //        File.WriteAllText($"{filePath}.txt", TxtTb.Text);
+        //        isTextChanged = false;
+        //    }
+        //    public void SaveJpgBut() //за пределами хранителя это работает
+        //    {
+        //        Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+        //        dlg.FileName = "savedimage";
+        //        dlg.DefaultExt = ".jpg";
+        //        dlg.Filter = "Image (.jpg)|*.jpg";
+
+        //        Nullable<bool> result = dlg.ShowDialog();
+
+        //        if (result == true)
+        //        {
+        //            string filename = dlg.FileName;
+        //            int margin = (int)this.IC.Margin.Left;
+        //            int width = (int)this.IC.ActualWidth - margin;
+        //            int height = (int)this.IC.ActualHeight - margin;
+        //            RenderTargetBitmap rtb =
+        //            new RenderTargetBitmap(width, height, 94d, 92d, PixelFormats.Default);
+        //            rtb.Render(IC);
+
+        //            using (FileStream fs = new FileStream(filename, FileMode.Create))
+        //            {
+        //                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+        //                encoder.Frames.Add(BitmapFrame.Create(rtb));
+        //                encoder.Save(fs);
+        //            }
+        //        }
+        //    }
+        //}
 
     }
 
-
-
-
 }
+
+
